7e35588c4c2bf704a646134e4a2a256ec01d4d59
Taker3141 <minecrafty512@gmail.com>
2017-08-27 21:00:57 +0200

    Created level structure

---
+++ src/level/Level.java
@@ -0,0 +1,36 @@
+package level;
+
+import level.LevelManager.Section;
+import org.lwjgl.util.vector.Vector3f;
+import world.World;
+
+public class Level
+{
+	public final String name;
+	public final Class<? extends World> startWorld;
+	public final Vector3f startPosition;
+	public boolean available;
+	
+	public Level(String name, Class<? extends World> startWorld, Vector3f startPosition, boolean available)
+	{
+		this.name = name;
+		this.startWorld = startWorld;
+		this.startPosition = startPosition;
+		this.available = available;
+	}
+	
+	public World loadLevel()
+	{
+		try
+		{
+			World w = startWorld.newInstance();
+			w.player.position = startPosition;
+			return w;
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return null;
+		}
+	}
+}
---
+++ src/level/LevelManager.java
@@ -0,0 +1,39 @@
+package level;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.lwjgl.util.vector.Vector3f;
+import world.MoonLabWorld;
+
+public class LevelManager
+{
+	public List<Section> sections = new ArrayList<>();
+	
+	public LevelManager()
+	{
+		loadProgress();
+		loadLevels();
+	}
+	
+	public class Section
+	{
+		public final String name;
+		public List<Level> levels = new ArrayList<>();
+		
+		private Section(String name)
+		{
+			this.name = name;
+		}
+	}
+	
+	private void loadLevels()
+	{
+		sections.add(new Section("Das Mond-Labor"));
+		sections.get(0).levels.add(new Level("Erstes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
+	}
+	
+	private void loadProgress()
+	{
+		
+	}
+}
