a6ad2c7e6c642220c54cbf0f5318cf98cd8e9eed
Taker3141 <minecrafty512@gmail.com>
2017-08-29 23:25:41 +0200

    Set up folder structure for level and section files

--- src/level/LevelManager.java
+++ src/level/LevelManager.java
@@ -28,6 +28,7 @@ public class LevelManager
 	
 	private void loadLevels()
 	{
+		//TODO Implement parser for level base File
 		sections.add(new Section("Das Mond-Labor"));
 		sections.get(0).levels.add(new Level("Erstes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
 		sections.get(0).levels.add(new Level("Zweites Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
@@ -41,7 +42,6 @@ public class LevelManager
 		sections.get(1).levels.add(new Level("1", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
 		sections.get(1).levels.add(new Level("2", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
 		sections.get(1).levels.add(new Level("3", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		
 	}
 	
 	private void loadProgress()
