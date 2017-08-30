e8db8c0be7a3a79564a191d71ea0051b954cb271
Taker3141 <minecrafty512@gmail.com>
2017-08-30 15:56:28 +0200

    Implemented loading of level names from section index files

--- src/level/LevelManager.java
+++ src/level/LevelManager.java
@@ -1,7 +1,15 @@
 package level;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import org.lwjgl.util.vector.Vector3f;
 import world.MoonLabWorld;
 
@@ -28,24 +36,48 @@ public class LevelManager
 	
 	private void loadLevels()
 	{
-		//TODO Implement parser for level base File
-		sections.add(new Section("Das Mond-Labor"));
-		sections.get(0).levels.add(new Level("Erstes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(0).levels.add(new Level("Zweites Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(0).levels.add(new Level("Drittes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(0).levels.add(new Level("Viertes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(0).levels.add(new Level("Fünftes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(0).levels.add(new Level("Sechstes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(0).levels.add(new Level("Siebtes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(0).levels.add(new Level("Achtes Level", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.add(new Section("Irgendwas"));
-		sections.get(1).levels.add(new Level("1", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(1).levels.add(new Level("2", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
-		sections.get(1).levels.add(new Level("3", MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
+		String levelPath = "res/level/";
+		File sectionIndexFile = new File(levelPath + "sections");
+		List<String> sectionNames = new ArrayList<>();
+		try (FileReader r = new FileReader(sectionIndexFile); BufferedReader reader = new BufferedReader(r))
+		{
+			String line = reader.readLine();
+			while (line != null)
+			{
+				sectionNames.add(line);
+				line = reader.readLine();
+			}
+		}
+		catch (Exception e)
+		{
+			System.out.println("Could not read level section index file");
+			e.printStackTrace();
+		}
+		for (String n : sectionNames)
+		{
+			sections.add(new Section(n));
+			try (FileReader r = new FileReader(levelPath + n + "/section_info"); BufferedReader reader = new BufferedReader(r))
+			{
+				String line = reader.readLine();
+				while (line != null)
+				{
+					if(line.startsWith("level:"))
+					{
+						sections.get(sections.size() - 1).levels.add(new Level(line.split(":")[1].trim(), MoonLabWorld.class, new Vector3f(1844, 21, 1623), true));
+					}
+					line = reader.readLine();
+				}
+			}
+			catch (Exception e)
+			{
+				System.out.println("Could not read section info file for section \'" + n + "\'");
+				e.printStackTrace();
+			}
+		}
 	}
 	
 	private void loadProgress()
-	{
+	{	
 		
 	}
 }
