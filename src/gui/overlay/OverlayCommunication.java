e2e784cdd406f4b9910d4f595e39cf369bfc113f
Taker3141 <minecrafty512@gmail.com>
2017-09-04 23:18:59 +0200

    Added lines of text to communications overlay

--- src/gui/overlay/OverlayCommunication.java
+++ src/gui/overlay/OverlayCommunication.java
@@ -2,18 +2,24 @@ package gui.overlay;
 
 import org.lwjgl.util.vector.Vector2f;
 import renderer.DisplayManager;
+import font.fontMeshCreator.GUIText;
 import gui.element.GuiElement;
 
 public class OverlayCommunication extends Overlay
 {
 	protected GuiElement background;
 	protected float vY = 0;
+	protected GUIText[] textLines = new GUIText[5];
 	
 	public OverlayCommunication()
 	{
 		position = new Vector2f(0, H -256);
 		background = new GuiElement(loader.loadTexture("texture/gui/communication/background"), position, new Vector2f(1024, 256), null);
-		
+		for(int i = 0; i < textLines.length; i++) 
+		{
+			textLines[i] = new GUIText("TEXT LINE " + i, 1.8F, font, textPosition(i), 734F / W, false);
+			textLines[i].setColour(0, 0.812F, 0.38F);
+		}
 		elements.add(background);
 	}
 	
@@ -24,27 +30,34 @@ public class OverlayCommunication extends Overlay
 	
 	public void hide()
 	{
-		if(vY == 0) vY = 150;
+		if(vY == 0) vY = 200;
 	}
 	
 	public void show()
 	{
-		if(vY == 0) vY = -150;
+		if(vY == 0) vY = -200;
+	}
+	
+	public void setText(String text, int line)
+	{
+		textLines[line].setText(text);
 	}
 	
 	public void update()
 	{
 		float dy = DisplayManager.getFrameTimeSeconds() * vY;
-		if(position.y > H - 256 && dy < 0)
-		{
-			position.y += dy;
-			background.position = position;
-		}
-		else if(position.y < H && dy > 0)
-		{
-			position.y += dy;
-			background.position = position;
-		}
+		if((position.y > H - 256 && dy < 0) || (position.y < H && dy > 0)) position.y += dy;
 		else vY = 0;
+		
+		if(dy != 0)
+		{
+			background.position = position;
+			for(int i = 0; i < textLines.length; i++) textLines[i].position = textPosition(i);
+		}
+	}
+	
+	private Vector2f textPosition(int line)
+	{
+		return new Vector2f(position.x + 263, (position.y + 230) - line * 41);
 	}
 }
