7e5b463d132a946284b480597c72672e6c02e8a7
Taker3141 <minecrafty512@gmail.com>
2017-10-07 14:54:03 +0200

    Fixed black border around scroll bar

--- src/gui/element/VerticalScrollBar.java
+++ src/gui/element/VerticalScrollBar.java
@@ -19,7 +19,7 @@ public class VerticalScrollBar extends GuiElement implements IClickable
 	{
 		super(0, position, size, parent);
 		texture = backgroundTexture;
-		slider = new GuiElement(sliderTexture, new Vector2f(position), new Vector2f(size.x, size.y / 4), parent);
+		slider = new GuiElement(sliderTexture, new Vector2f(position), new Vector2f(size.x, (size.y / 4) * 0.6875F), parent);
 	}
 	
 	public void addComponents(List<GuiElement> list)
@@ -30,8 +30,7 @@ public class VerticalScrollBar extends GuiElement implements IClickable
 	
 	public float getSlide()
 	{
-		final float offsetTop = ((size.y / 4) / 64) * 16;
-		final float maxDistance = size.y - slider.size.y + offsetTop;
+		final float maxDistance = size.y - slider.size.y;
 		final float distance = slider.position.y - position.y;
 		return distance / maxDistance;
 	}
@@ -39,8 +38,7 @@ public class VerticalScrollBar extends GuiElement implements IClickable
 	public void setSlide(float slide)
 	{
 		slide = slide > 1 ? 1 : slide < 0 ? 0 : slide;
-		final float offsetTop = ((size.y / 4) / 64) * 16;
-		final float maxDistance = size.y - slider.size.y + offsetTop;
+		final float maxDistance = size.y - slider.size.y;
 		final float distance = slide * maxDistance;
 		slider.position.y = position.y + distance;
 	}
@@ -74,10 +72,9 @@ public class VerticalScrollBar extends GuiElement implements IClickable
 		{
 			ret = true;
 			y = Display.getHeight() - y;
-			final float offsetTop = ((size.y / 4) / 64) * 16;
 			float newY = Display.getHeight() - y - slider.size.y / 2;
 			if (newY < position.y) newY = position.y;
-			if (newY + slider.size.y - offsetTop > position.y + size.y) newY = position.y + size.y - slider.size.y + offsetTop;
+			if (newY + slider.size.y > position.y + size.y) newY = position.y + size.y - slider.size.y;
 			slider.position.y = newY;
 			if(handler != null) handler.click(parent);
 		}
